0fad398641101187e5b132b53cee3815a5a204ae
SyStEm-InFeT <systeminfet94@gmail.com>
2012-01-30 12:57:02 +0100

    Versione 2.0 riscritta totalmente :D:D usando i file xml come database :D:D

--- firewall.php
+++ firewall.php
@@ -1,7 +1,8 @@
 <?php
 
 foreach ($_GET as $getRequest) {
-    if ((preg_match("/[:alpha:]/i", $getRequest)) OR (preg_match("/[:punct:]/i", $getRequest))) {
+    if (($getRequest != $_GET['seo']) && (preg_match("/[:alpha:]/i", $getRequest)) OR (preg_match("/[:punct:]/i", $getRequest))) {
         print"<h1>YOU FAIL STUPID LAMER!!!</h1>";
     }
 }
+?>
