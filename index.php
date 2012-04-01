bed004c889f003b742d3992c92645fae4d39b0aa
SyStEm-InFeT <systeminfet94@gmail.com>
2012-04-01 15:58:41 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,6 @@ die(header("Location: install.php"));
 }
 require_once("config.php");
 pageOpen("Home Page", "Home Page");
-print"".html_entity_decode(file_get_contents("".FULL_PATH."database/pagine/home.html"))."";
+print"".stripslashes(html_entity_decode(file_get_contents("".FULL_PATH."database/pagine/home.html")))."";
 pageClose();
 ?>
